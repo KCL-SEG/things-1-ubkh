0450dee966f209a2b82e3f0c2e23014aeca821ff
Ubayd Khan <ubaydkh@gmail.com>
2022-12-09 14:39:38 +0000

    Attempt to fix length error

--- things/models.py
+++ things/models.py
@@ -4,8 +4,8 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.TextField(unique=True, blank=False, max_length=30)
-    description = models.TextField(unique=False, blank=True, max_length=120)
+    name = models.CharField(unique=True, blank=False, max_length=30)
+    description = models.CharField(unique=False, blank=True, max_length=120)
     quantity = models.IntegerField(
         unique=False,
         validators=[MaxValueValidator(100), MinValueValidator(0)]
